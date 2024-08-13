191200e458755bf3ad5b8b2d1765496781d91337
Dharineesh S <dharineesh22@gmail.com>
2024-08-13 15:49:16 +0530

    Add files via upload

---
+++ 800/GameWithIntegers.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+    while(t--){
+       int n;
+       cin >> n;
+       if(n%3>0){
+           cout << "First" << endl;
+       }
+       else{
+           cout << "Second" << endl;
+       }
+    }
+    return 0;
+}
